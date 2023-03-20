344429d8012c868686f3e6331619fdeb7da58188
RaffoMatto <61514052+RaffoMatto@users.noreply.github.com>
2023-03-19 22:52:31 -0300

    Ajuste de animações (#6)
    * Figura dos ponteiros, hover nos botões e divisória

--- js/main.js
+++ js/main.js
@@ -78,6 +78,7 @@ const pauseTimer = function () {
 const stopTimer = function () {
     btnLapStop.style = `
     left: 50%;
+    opacity: 0;
     transform: translate(-50%, -50%);`
     
     btnPlayPause.style = `
@@ -90,11 +91,11 @@ const stopTimer = function () {
 
 const resetTimer = function () {
     if (globalTimer) {
+        clearInterval(globalTimer);
         globalTimer = null;
-        labelTimer.innerText = '00:00.00';
+        labelTimer.innerText = '00:00:00.00';
         laps = [];
         time = 0;
-        clearInterval(globalTimer);
         resetLapTimer();
     }
 }
@@ -120,6 +121,7 @@ const changeToPlay = function (){
     if (!globalTimer){
         btnLapStop.style = `
             left: 0;
+            opacity: 1;
             transform: translate(0, -50%);`
         btnPlayPause.style = `
             left: 100%;
